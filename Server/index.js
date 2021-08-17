07026b9f82892d80b560f374f561dbf5238f42b7
JonHimself <jcbrstudio@gmail.com>
2021-08-17 14:35:34 -0500

    backend 1

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get('/api/users', (req, res) => {
+    let friends = ['Lexi', 'Caleb', 'Carl', 'Tuyen', 'Ariadne', 'Ben'];
+    res.status(200).send(friends);
+})
+
+app.get('/weather/:temperature', (req, res) => {
+    const temperature = req.params.temperature;
+    const phrase = `<h3>It was ${temperature} today</h3>`;
+    res.status(200).send(phrase);
+})
+
+
+
+app.listen(4000, () => {
+    console.log('We are running on port 4000 homeboy');
+});
